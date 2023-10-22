eb3d8c1509c1f54e176267f8d8e94377ac3f54b7
Alejo Muñoz <alejandro.munoz@imagineapps.co>
2023-10-22 14:21:51 -0500

    Register Cities Repository

--- lib/global_locator.dart
+++ lib/global_locator.dart
@@ -2,6 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:just_play_test/src/api/api_repository.dart';
 import 'package:just_play_test/src/bloc/auth/auth_bloc.dart';
 import 'package:just_play_test/src/bloc/cities/cities_bloc.dart';
+import 'package:just_play_test/src/repository/cities/cities_repository.dart';
 
 late GetIt global;
 
@@ -9,6 +10,8 @@ void setUpGlobalLocator() {
   global = GetIt.I;
 
   global.registerFactory<APIRepository>(() => DefaultAPIRepository());
+  global.registerFactory<CitiesRepository>(() => CitiesDefault());
+
   global.registerLazySingleton<AuthBloc>(() => AuthBloc());
   global.registerLazySingleton<CitiesBloc>(() => CitiesBloc());
 }
