d2859784e6dd9e1fa36160e8cbcac43278b97bd0
Alejo Muñoz <alejandro.munoz@imagineapps.co>
2023-10-22 12:12:33 -0500

    register cities bloc

--- lib/global_locator.dart
+++ lib/global_locator.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:just_play_test/src/api/api_repository.dart';
 import 'package:just_play_test/src/bloc/auth/auth_bloc.dart';
+import 'package:just_play_test/src/bloc/cities/cities_bloc.dart';
 
 late GetIt global;
 
@@ -9,4 +10,5 @@ void setUpGlobalLocator() {
 
   global.registerFactory<APIRepository>(() => DefaultAPIRepository());
   global.registerLazySingleton<AuthBloc>(() => AuthBloc());
+  global.registerLazySingleton<CitiesBloc>(() => CitiesBloc());
 }
