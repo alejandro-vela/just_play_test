47298de7bbac033bd380eb6c8e2f5193c1b815ae
Alejo Muñoz <alejandro.munoz@imagineapps.co>
2023-10-22 14:22:45 -0500

    add local storage

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:just_play_test/firebase_options.dart';
 import 'package:just_play_test/global_locator.dart';
 import 'package:just_play_test/src/bloc/observer_bloc.dart';
+import 'package:just_play_test/src/services/local_storage.dart';
 
 import 'src/theme/theme.dart';
 import 'src/ui/splash_screen/splash_screen.dart';
@@ -11,6 +12,7 @@ import 'src/ui/splash_screen/splash_screen.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = ObserverBloc();
+  await LocalStorage.configurePrefs();
   setUpGlobalLocator();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
